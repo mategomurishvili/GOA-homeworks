3016320d30a5ec5544fab67ea44169fbe1298075
mategomurishvili <mategomurishvili65@gmail.com>
2024-02-04 08:58:46 -0800

    first commit

---
+++ day 1/day1.py
@@ -0,0 +1,18 @@
+surname = "gomurishvili"
+
+name = "mate"
+
+print(name)
+
+height = 172.5
+
+age = 15
+
+knows_programming= True
+is_ugly = False
+
+isUgly = False
+
+print(name + " " + surname)
+
+print(name + " " + str(age))
