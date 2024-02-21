1a3e8922005a035ff9f3c8a12855c0263f6239a4
mategomurishvili <mategomurishvili65@gmail.com>
2024-02-21 00:52:42 -0800

    commit

---
+++ day 05/homework.py
@@ -0,0 +1,29 @@
+num1=int(input("Please enter first number: "))
+num2=int(input("Please enter second number: "))
+num3=num1 + num2
+print(num3)
+
+num1=int(input("Please enter first number: "))
+num2=int(input("Please enter second number: "))
+num3=num1 - num2
+print(num3)
+
+num1=int(input("Please enter first number: "))
+num2=int(input("Please enter second number: "))
+num3=num1 * num2
+print(num3)
+
+num1=int(input("Please enter first number: "))
+num2=int(input("Please enter second number: "))
+num3=num1 / num2
+print(num3)
+
+num1=int(input("Please enter first number: "))
+num2=int(input("Please enter second number: "))
+num3=num1 < num2
+print(num3)
+
+num1=int(input("Please enter first number: "))
+num2=int(input("Please enter second number: "))
+num3=num1 > num2
+print(num3)
